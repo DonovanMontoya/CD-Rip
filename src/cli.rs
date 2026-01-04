016834032e30dec69e4fdd267dc176ab9c27d267
Donovan Montoya <donovan.montoya1@gmail.com>
2026-01-04 16:02:59 -0500

    add `clap` dependency and implement CLI commands for `rip` and `view`

---
+++ src/cli.rs
@@ -0,0 +1,27 @@
+use clap::{Parser, Subcommand, command};
+use std::path::{Path, PathBuf};
+
+#[derive(Parser)]
+#[command(name = "cdrip")]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Locate and Rip aiff files from CD
+    Rip {
+        /// Output Path( /Desktop/CDName )
+        #[arg(short, long)]
+        path: PathBuf,
+    },
+
+    /// Shows contents of CD
+    View {
+        /// Show contents for the specific cd Dir
+        #[arg(short, long)]
+        media: Option<String>,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use crate::cli::{Cli, Commands};
+use clap::Parser;
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Rip { path } => {
+            println!("Rip: output path: {:?}", path);
+        }
+        Commands::View { media } => {
+            println!("View: device: {:?}", media);
+        }
+    }
 }
